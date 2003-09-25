58861bf47e1be65ed7d9e314f99164c2059c70e1
Steffen Winterfeldt <wfeldt@opensuse.org>
2003-09-25 10:19:35 +0000

    - added OpenSchool theme

--- lib/ReadConfig.pm
+++ lib/ReadConfig.pm
@@ -621,6 +621,13 @@ for (@f) {
     $update_dir = "/linux/UnitedLinux/$real_arch-$ul_release";
     $load_image = 96*1024;
   }
+  elsif($theme eq "OpenSchool") {
+    $yast_theme = "SuSELinux";
+    $splash_theme = "OpenSchool";
+    $product_name = "SuSE Linux";
+    $update_dir = "/linux/UnitedLinux/$real_arch-$ul_release";
+    $load_image = 96*1024;
+  }
   else {
     die "don't know theme \"$theme\""
   }
