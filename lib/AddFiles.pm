414565218bebec6957e293e0044998171fa409a7
Steffen Winterfeldt <wfeldt@opensuse.org>
2007-02-21 15:59:08 +0000

    - don't stop if kmp packages are missing

--- lib/AddFiles.pm
+++ lib/AddFiles.pm
@@ -374,7 +374,7 @@ sub AddFiles
             for $kmp (split(',', $ConfigData{kmp_list})) {
               ($kmp_name = $p) =~ s/^kernel/$kmp-kmp/;
               my $r2 = RPMFileName "$kmp_name";
-              warn("$Script: no such package: ${kmp_name}.rpm"), next unless $r2 && -f $r2;
+              (print STDERR "$Script: no such package: ${kmp_name}.rpm\n"), next unless $r2 && -f $r2;
               SUSystem "sh -c 'cd $tdir ; rpm2cpio $r2 | cpio --quiet --sparse -dimu --no-absolute-filenames'" and
                 warn "$Script: failed to extract $r2";
             }
