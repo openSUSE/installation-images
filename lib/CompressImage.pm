2f06b4dcb2e908532594a4b8a03002983abd2940
Steffen Winterfeldt <wfeldt@opensuse.org>
2014-01-21 14:00:37 +0100

    use xz for compression

--- lib/CompressImage.pm
+++ lib/CompressImage.pm
@@ -18,11 +18,13 @@ use integer;
 sub CompressImage
 {
   local $_;
-  my ($image, $name, $size, $image2);
+  my ($image, $name, $size, $image2, $prog, $prog_opt);
 
   $name = $image = shift;
 
   $name =~ s#^.*/##;
+
+  $prog = shift || 'gzip';
 
   $size = -s $image;
 
@@ -34,9 +36,13 @@ sub CompressImage
 
   print "compressing $image...\n";
 
-  die "$Script: gzip failed" if system "gzip -f9N '$image2'";
+  $prog_opt = '-cf9N' if $prog eq 'gzip';
+  $prog_opt = '-cf' if $prog eq 'xz';
 
-  die "$Script: $!" unless rename "$image2.gz", $image;
+  die "$Script: $prog failed" if system "$prog $prog_opt '$image2' >'$image2.tmp'";
+
+  die "$Script: $!" unless rename "$image2.tmp", $image;
+  die "$Script: $!" unless unlink $image2;
 
   return -s $image;
 }
