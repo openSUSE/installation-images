ec385541b8f888b9a74cd554ad0396406535a823
Dinar Valeev <dvaleev@suse.com>
2014-01-23 10:13:18 +0100

    We're in transition to grub2, we build yaboot binary only for chrp.

--- bin/mk_ppc_installation-images_bootbinaries.sh
+++ bin/mk_ppc_installation-images_bootbinaries.sh
@@ -35,7 +35,7 @@ CD1=$targetdir/CD1
 mkdir -pv $CD1/ppc
 # to trigger the HFS part, avoid 8.3 filenames and allow OF booting
 mkdir -pv $CD1/suseboot
-cp -pfv /lib/lilo/pmac/yaboot           $CD1/suseboot/yaboot
+cp -pfv /lib/lilo/chrp/yaboot           $CD1/suseboot/yaboot
 cp -pfv /lib/lilo/chrp/yaboot.chrp      $CD1/suseboot/yaboot.ibm
 if test "$do_32" = "true" ; then
 # provide PS3 bootloader only on openSuSE
